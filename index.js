b572d46b403bb37b00e6eced422bf62f0645bec7
+ <+>
2023-04-23 12:57:22 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainNode = document.querySelector('#main');
+mainNode.parentNode.removeChild(mainNode);
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'Your Victory Message Goes Here';
+newHeader.textContent = `${`Chris`} is the champion`;
